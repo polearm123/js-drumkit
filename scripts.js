1dfaf9bbc8e45534083cd018318021251422519b
polearm123 <shaggywaggy29@gmail.com>
2020-11-06 11:11:10 +0000

    added event listeners for transition end, there is now complete animation on press and release

--- scripts.js
+++ scripts.js
@@ -1,14 +1,42 @@
 //listen for a key press
 //depending on the key, activate the sound connected to that key press
 
+function removeTransition(e){
 
-window.addEventListener('keydown', (e) => {
-    console.log(e.keyCode);
+    if(e.propertyName !== 'transform') return;
+
+    console.log(e.propertyName);
+    this.classList.remove('playing'); //this is the current key
+
+
+
+
+
+}
+function playSound(e) {
+    
     const audioSelector = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    console.log(audioSelector);
-    if(!audioSelector) return; //if there is no audio with that datakey return with no sound
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
+    // key.setAttribute("style","border: 5px solid red;")
+    // key.style.text
+
+    
+    if(!audioSelector) return; //if there is no audio with that datakey return with no sound
+    
+    audioSelector.currentTime = 0; //
     audioSelector.play();
 
-})
+    key.classList.add('playing'); //adds the playing css elements to the key
+
+}
+
+window.addEventListener('keydown', playSound);
+
+const keys = document.querySelectorAll(".key");
+
+
+keys.forEach((key) => key.addEventListener('transitionend',removeTransition)); //adds a listener to each key
+
+
 
